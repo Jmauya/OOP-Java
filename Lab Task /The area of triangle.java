5817a26f654d4d30add619d794b17479a630f973
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2025-09-26 01:01:25 +0600

    The area of Triangle.java

---
+++ Lab Task /The area of triangle.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main { 
+public static void main (String [] args ) {
+  Scanner sc = new Scanner ( System.in);
+  System.out.print ("Enter the base of the triangle: ");
+  double.base = sc.nextDouble ();
+  System.out.print ("Enter the height of the triangle: ") ;
+  double.height = sc.nextDouble ();
+
+  double a = 0.5* base * height;
+
+  System.out.println("the area of the triangle: "  +a);
+
+  sc.close(); 
+} 
