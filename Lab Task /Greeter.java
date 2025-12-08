779714d4b2b1f86adc4f530bd17cfda3d7d345b6
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2025-12-08 23:35:09 +0600

    Greeter.java

--- Lab Task /Greeter.java
+++ Lab Task /Greeter.java
@@ -1,3 +1,4 @@
+//main class
 package oop;
 
 public class Main {
@@ -9,5 +10,27 @@ public class Main {
         g.greet();                     // no input
         g.greet("Liam");               // name only
         g.greet("Sophia", 22);         // name + age
+    }
+}
+
+
+//
+package oop;
+
+public class Greeter {
+
+    // 1. Greet without input
+    void greet() {
+        System.out.println("Hello! Nice to meet you!");
+    }
+
+    // 2. Greet with name only
+    void greet(String name) {
+        System.out.println("Hello " + name + "! Nice to meet you!");
+    }
+
+    // 3. Greet with name and age
+    void greet(String name, int age) {
+        System.out.println("Hello " + name + "! You are " + age + " years old!");
     }
 }
