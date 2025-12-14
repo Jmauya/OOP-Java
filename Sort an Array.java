3d6ef600333d4c927bb95c668704fb0b80d0ccb9
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2025-12-14 22:09:50 +0600

    Sort an Array.java

---
+++ Sort an Array.java
@@ -0,0 +1,14 @@
+
+import java.util.Arrays;                                                                   // Import Arrays utility class
+
+public class SortArray {     // Define the class (no space allowed in name)
+    
+    public static void main(String[] args) {        // Main method
+        
+        int[] arr = {190, 62, 33, 24}; // Declare and initialize array
+        
+        Arrays.sort(arr); // Sort the array in ascending order
+        
+        System.out.println(Arrays.toString(arr)); // Print the sorted array
+    }
+}
