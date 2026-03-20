c0ca5d18e6801329c685eefb61310384c696c113
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-03-20 23:43:42 +0600

    Sort An Array.java

--- Sort An Array.java
+++ Sort An Array.java
@@ -5,7 +5,7 @@ public class SortArray {   //Define the class (no space allowed in name)
     
     public static void main(String[] args) {      // Main method
         
-        int[] arr = {190, 62, 33, 24}; // Declare and initialize array
+        int[] arr = {190, 62, 33, 24};    // Declare and initialize array
         
         Arrays.sort(arr); // Sort the array in ascending order
         
