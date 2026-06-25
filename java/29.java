94cee34ab36bb989549054ce12c64fe0fae549df
Jannatul Mauya Nabila <jannatulmauyanabila@gmail.com>
2026-06-25 23:13:49 +0600

    29.java

---
+++ java/29.java
@@ -0,0 +1,16 @@
+@Test
+void isValidUsername_returnsFalse_whenShortUsername() {
+    // Enter a username that is too short
+    String username = "ab";
+    boolean actual = UsernameValidator.isValidUsername(username);
+    assertFalse(actual);
+}
+
+@Test
+void isValidUsername_returnsFalse_whenNull() {
+    String username = null;
+    // Pass the username to isValidUsername 
+    boolean actual = UsernameValidator.isValidUsername(username);
+    // Verify the username is invalid
+    assertFalse(actual);
+}
