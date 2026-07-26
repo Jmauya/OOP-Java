8111ec8c3cf84fc104125d1cad56c9013fe3abb2
Jannatul Mauya <jannatulmauyanabila@gmail.com>
2026-07-26 23:05:38 +0600

    30.java

--- java/30.java
+++ java/30.java
@@ -5,17 +5,14 @@ public class BufferedReaderExample {
         String filePath = "sample_market_data.csv";
         
         // Create a reader using chained constructors
-        
         try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             // Read and print the header line
             String header = reader.readLine();
             System.out.println("Header: " + header);
             
             // Read and print the first data row
-            
             String dataRow = reader.readLine();
             System.out.println("First data row: " + dataRow);
-            
             System.out.println("Successfully read from file using BufferedReader");
         } catch (IOException e) {
             System.err.println("Error reading file: " + e.getMessage());
