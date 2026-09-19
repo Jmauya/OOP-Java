d0d430d40b1290df05c2c0fe25400ce409d3cf26
Jannatul Mauya <jannatulmauyanabila@gmail.com>
2026-09-19 21:24:40 +0600

    Tablesaw (Java's data processing library).java

--- java/Tablesaw (Java's data processing library).java
+++ java/Tablesaw (Java's data processing library).java
@@ -5,11 +5,13 @@ public class GroceryDataQuality {
         for (String colName : inventory.columnNames()) {
             
             // Extract the column and count missing values in the column
+            
             int missing = inventory.column(colName).countMissing();
             System.out.println(colName + " missing values: " + missing);
         }
 
         // Count the inventory by unique categories
+        
         Table catCounts = inventory.countBy("Category");
         System.out.println("\nCategory distribution:");
         System.out.println(catCounts);
