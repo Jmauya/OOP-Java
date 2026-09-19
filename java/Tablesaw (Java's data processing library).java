b1ea5ab6ef946badeb5e2379d5b0b1630946a63c
Jannatul Mauya <jannatulmauyanabila@gmail.com>
2026-09-19 21:24:23 +0600

    Tablesaw (Java's data processing library).java

--- java/Tablesaw (Java's data processing library).java
+++ java/Tablesaw (Java's data processing library).java
@@ -3,6 +3,7 @@ public class GroceryDataQuality {
         Table inventory = Table.read().csv("grocery_inventory.csv");
 
         for (String colName : inventory.columnNames()) {
+            
             // Extract the column and count missing values in the column
             int missing = inventory.column(colName).countMissing();
             System.out.println(colName + " missing values: " + missing);
